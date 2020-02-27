f1b6ea898e88340facd387be798c4015a509cafd
XCarrel <Xavier.Carrel@cpnv.ch>
2020-02-27 18:09:50 +0100

    Crud & test done

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
 require "crud.php";
-$dbh = getPDO();
-$query = "SELECT count(*) as nb FROM filmmakers";
-$statment = $dbh->prepare($query);//prepare query
-$statment->execute();//execute query
-$queryResult = $statment->fetch(PDO::FETCH_ASSOC);//prepare result for client
-extract($queryResult);
 
-echo "nb = $nb\n";
 ?>
--- testcrud.php
+++ testcrud.php
@@ -11,48 +11,39 @@ function testGetAllFilmMakers()
     echo "Test unitaire de la fonction getAllItems : ";
     $items = getAllFilmMakers();
 
-    if (count($items) == 4)
-    {
+    if (count($items) == 4) {
         echo 'OK !!!';
-    }
-    else
-    {
+    } else {
         echo 'BUG !!!';
     }
     echo "\n";
 }
 
-function testGetFilmMaker($id)
+function testGetFilmMaker()
 {
     echo "Test unitaire de la fonction getItem : ";
     $item = getFilmMaker(3);
-    if ($item['firstname'] == 'Luc-Olivier')
-    {
+    if ($item['firstname'] == 'Luc-Olivier') {
         echo 'OK !!!';
-    }
-    else
-    {
+    } else {
         echo 'BUG !!!';
     }
     echo "\n";
 }
 
-function testGetFilmMakerByName($name)
+function testGetFilmMakerByName()
 {
-    echo "Test unitaire de la fonction getFilmMakerName : ";
+    echo "Test unitaire de la fonction getFilmMakerByName : ";
     $item = getFilmMakerByName('Chamblon');
-    if ($item['id'] == 3)
-    {
+    if ($item['id'] == 3) {
         echo 'OK !!!';
-    }
-    else
-    {
+    } else {
         echo '### BUG ###';
     }
     echo "\n";
 }
 
-function testUpdateFilmMaker($filmMaker)
+function testUpdateFilmMaker()
 {
     echo "Test unitaire de la fonction updateFilmMaker : ";
     $item = getFilmMakerByName('Chamblon');
@@ -61,13 +52,56 @@ function testUpdateFilmMaker($filmMaker)
     $item['lastname'] = 'Menfain';
     updateFilmMaker($item);
     $readback = getFilmMaker($id);
-    if (($readback['firstname'] == 'Gérard') && ($readback['lastname'] == 'Menfain'))
-    {
+    if (($readback['firstname'] == 'Gérard') && ($readback['lastname'] == 'Menfain')) {
         echo 'OK !!!';
-    }
-    else
-    {
+    } else {
         echo '### BUG ###';
+    }
+    echo "\n";
+}
+
+function testCreateFilmMaker()
+{
+    echo "Test unitaire de la fonction createFilmMaker : ";
+    $item = [
+        'firstname' => 'Paulo',
+        'lastname' => 'Gramm',
+        'filmmakersnumber' => '54321',
+        'birthname' => '1987-12-21',
+        'nationality' => 'FR'
+    ];
+    $nfm = countFilmMakers();
+    $newFilmMaker = createFilmMaker($item);
+    if (countFilmMakers() == $nfm + 1) {
+        $readback = getFilmMaker($newFilmMaker['id']);
+        if (($readback['firstname'] == 'Paulo') && ($readback['lastname'] == 'Gramm') && ($readback['filmmakersnumber'] == 54321) && ($readback['birthname'] == '1987-12-21') && ($readback['nationality'] == 'FR')) {
+            echo 'OK !!!';
+        } else {
+            echo '### BUG (diff) ###';
+        }
+    } else {
+        echo '### BUG (count) ###';
+    }
+    echo "\n";
+}
+
+function testDeleteFilmMaker()
+{
+    echo "Test unitaire de la fonction deleteFilmMaker : ";
+    $onefm = getFilmMakerByName('Gramm');
+    $nfm = countFilmMakers();
+    if (deleteFilmMaker($onefm['id'])) {
+        if (countFilmMakers() == $nfm - 1) {
+            if (getFilmMaker($onefm['id']) == null) {
+                echo 'OK !!!';
+            } else {
+                echo '### BUG (no delete) ###';
+            }
+        } else {
+            echo '### BUG (count) ###';
+        }
+    } else {
+        echo '### BUG (function) ###';
     }
     echo "\n";
 }
@@ -85,3 +119,5 @@ testGetFilmMaker();
 testGetFilmMakerByName();
 testGetAllFilmMakers();
 testUpdateFilmMaker();
+testCreateFilmMaker();
+testDeleteFilmMaker();
